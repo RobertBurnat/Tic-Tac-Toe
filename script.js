ce715c6a34a479bd28f915866fb561a9bcd53b74
Robert Burnat <burnat.r@sunex.pl>
2018-07-27 09:36:20 +0200

    pulled

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 (function() {
     let poles = Array.prototype.slice.call(document.querySelectorAll(".pole"));
+    console.log(poles);
     let activePlayer = 0;
     let disabledPole = [];
     var round = 0;
@@ -9,16 +10,17 @@
         for(let i = 0, size = poles.length; i < size; i++) {
             let pole = document.getElementById([i +1]);
             pole.addEventListener('click', function() {
-                let disablePole = poles[i].classList.add('disable');
                 if(activePlayer === 0) {
                     poles[i].innerHTML = 'X';
+                    poles[i].classList.add('disable');
                     nextPlayer();
                     checkWinner();
                 } else {
-                    poles[i].innerHTML = 'O';
-                    nextPlayer();
-                    checkWinner();
-                }
+                poles[i].innerHTML = 'O';
+                poles[i].classList.add('disable');
+                nextPlayer();
+                checkWinner();
+            }
         });
     }
 })();
