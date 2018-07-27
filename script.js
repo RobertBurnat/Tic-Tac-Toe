ea5df6ca841413a6695536e5aa74d4e19eb82335
Robert Burnat <burnat.r@sunex.pl>
2018-07-27 09:42:19 +0200

    Add posibilites wins to variable

--- script.js
+++ script.js
@@ -29,47 +29,61 @@
     }
     
     function checkWinner() {
-        var player1 = 'X';
-        var player2 = 'O'
-        if((poles[0].innerHTML === player1 && poles[1].innerHTML === player1 && poles[2].innerHTML === player1)
-          ||
-          (poles[3].innerHTML === player1 && poles[4].innerHTML === player1 && poles[5].innerHTML === player1)
-          ||
-          (poles[6].innerHTML === player1 && poles[7].innerHTML === player1 && poles[8].innerHTML === player1)
-          ||
-          (poles[0].innerHTML === player1 && poles[3].innerHTML === player1 && poles[6].innerHTML === player1)
-          ||
-          (poles[1].innerHTML === player1 && poles[4].innerHTML === player1 && poles[7].innerHTML === player1)
-          ||
-          (poles[2].innerHTML === player1 && poles[5].innerHTML === player1 && poles[8].innerHTML === player1)
-          ||
-          (poles[0].innerHTML === player1 && poles[4].innerHTML === player1 && poles[8].innerHTML === player1)
-          ||
-          (poles[2].innerHTML === player1 && poles[4].innerHTML === player1 && poles[6].innerHTML === player1)) {
-            alert('Player One WIN');
-            restart.classList.remove('hide');
+        let matrix = [
+            //horizontal
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            //Vertical
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            //Diagonal
+            [0, 4, 8],
+            [2, 4, 6]
             
-        } else if((poles[0].innerHTML === player2 && poles[1].innerHTML === player2 && poles[2].innerHTML === player2)
-          ||
-          (poles[3].innerHTML === player2 && poles[4].innerHTML === player2 && poles[5].innerHTML === player2)
-          ||
-          (poles[6].innerHTML === player2 && poles[7].innerHTML === player2 && poles[8].innerHTML === player2)
-          ||
-          (poles[0].innerHTML === player2 && poles[3].innerHTML === player2 && poles[6].innerHTML === player2)
-          ||
-          (poles[1].innerHTML === player2 && poles[4].innerHTML === player2 && poles[7].innerHTML === player2)
-          ||
-          (poles[2].innerHTML === player2 && poles[5].innerHTML === player2 && poles[8].innerHTML === player2)
-          ||
-          (poles[0].innerHTML === player2 && poles[4].innerHTML === player2 && poles[8].innerHTML === player2)
-          ||
-          (poles[2].innerHTML === player2 && poles[4].innerHTML === player2 && poles[6].innerHTML === player2)) {
-            alert('Player Two WIN');
-            restart.classList.remove('hide');
-        } else if(round >= 9) {
-            alert('DRAW');
-            restart.classList.remove('hide');
-        }
+        ]
+//        var player1 = 'X';
+//        var player2 = 'O'
+//        if((poles[0].innerHTML === player1 && poles[1].innerHTML === player1 && poles[2].innerHTML === player1)
+//          ||
+//          (poles[3].innerHTML === player1 && poles[4].innerHTML === player1 && poles[5].innerHTML === player1)
+//          ||
+//          (poles[6].innerHTML === player1 && poles[7].innerHTML === player1 && poles[8].innerHTML === player1)
+//          ||
+//          (poles[0].innerHTML === player1 && poles[3].innerHTML === player1 && poles[6].innerHTML === player1)
+//          ||
+//          (poles[1].innerHTML === player1 && poles[4].innerHTML === player1 && poles[7].innerHTML === player1)
+//          ||
+//          (poles[2].innerHTML === player1 && poles[5].innerHTML === player1 && poles[8].innerHTML === player1)
+//          ||
+//          (poles[0].innerHTML === player1 && poles[4].innerHTML === player1 && poles[8].innerHTML === player1)
+//          ||
+//          (poles[2].innerHTML === player1 && poles[4].innerHTML === player1 && poles[6].innerHTML === player1)) {
+//            alert('Player One WIN');
+//            restart.classList.remove('hide');
+//            
+//        } else if((poles[0].innerHTML === player2 && poles[1].innerHTML === player2 && poles[2].innerHTML === player2)
+//          ||
+//          (poles[3].innerHTML === player2 && poles[4].innerHTML === player2 && poles[5].innerHTML === player2)
+//          ||
+//          (poles[6].innerHTML === player2 && poles[7].innerHTML === player2 && poles[8].innerHTML === player2)
+//          ||
+//          (poles[0].innerHTML === player2 && poles[3].innerHTML === player2 && poles[6].innerHTML === player2)
+//          ||
+//          (poles[1].innerHTML === player2 && poles[4].innerHTML === player2 && poles[7].innerHTML === player2)
+//          ||
+//          (poles[2].innerHTML === player2 && poles[5].innerHTML === player2 && poles[8].innerHTML === player2)
+//          ||
+//          (poles[0].innerHTML === player2 && poles[4].innerHTML === player2 && poles[8].innerHTML === player2)
+//          ||
+//          (poles[2].innerHTML === player2 && poles[4].innerHTML === player2 && poles[6].innerHTML === player2)) {
+//            alert('Player Two WIN');
+//            restart.classList.remove('hide');
+//        } else if(round >= 9) {
+//            alert('DRAW');
+//            restart.classList.remove('hide');
+//        }
     }
     
 })();
