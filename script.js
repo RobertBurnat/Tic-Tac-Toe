f062ea076b13873df8329adf8897374016106145
Robert Burnat <burnat.robertt@gmail.com>
2018-07-28 12:49:24 +0200

    Little fixes

--- script.js
+++ script.js
@@ -4,72 +4,79 @@
     restartBtn = document.querySelector('.restart')
     init();
     let game = (function game() {
+        testing();
         if(gamePlaying) {
-        for(let i = 0, size = poles.length; i < size; i++) {
-            poles[i].style.backgroundColor = '#ccc';
-            let pole = document.getElementById([i +1]);
-            pole.addEventListener('click', () => {
-                if(gamePlaying) {
-                poles[i].style.backgroundColor = '#fff';
-                poles[i].classList.add('disable');
-                if(activePlayer === 1) {
-                    poles[i].innerHTML = 'X';
-                    checkWinner();
-                } else {
-                    poles[i].innerHTML = 'O';
-                    checkWinner();
-                }
+                for(let i = 0, size = poles.length; i < size; i++) {
+                    poles[i].style.backgroundColor = '#ccc';
+                    let pole = document.getElementById([i +1]);
+                    pole.addEventListener('click', () => {
+                        if(gamePlaying) {
+                        poles[i].style.backgroundColor = '#fff';
+                        poles[i].classList.add('disable');
+                        if(activePlayer === 1) {
+                            poles[i].innerHTML = 'O';
+                            checkWinner();
+                        } else {
+                            poles[i].innerHTML = 'X';
+                            checkWinner();
+                        }
+                    }
+                });
             }
-        });
-    }
-}
+        }
 })();
     
     function nextPlayer() {
         if(gamePlaying) {
-        activePlayer === 2 ? activePlayer = 1 : activePlayer = 2;
-        round++;
+            activePlayer === 1 ? activePlayer = 2 : activePlayer = 1;
+            console.log(activePlayer);
+            round++;
         }
     }
     
     function checkWinner() {
         if(gamePlaying) {
-        let matrix = [
-            //horizontal
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            //Vertical
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            //Diagonal
-            [0, 4, 8],
-            [2, 4, 6]
-        ]
-        for(let i in matrix) {
-            let items = matrix[i].map(key => poles[key]);
-            if(items.every(itm => itm.innerHTML === "X") || items.every(itm => itm.innerHTML === "O")) {
-                const markup = `<h1 class="result_banner">Player ${activePlayer} Win!</h1>`;
-                items.every(itm => itm.style.backgroundColor = '#22dc2e');
+            let matrix = [
+                //horizontal
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                //Vertical
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                //Diagonal
+                [0, 4, 8],
+                [2, 4, 6]
+            ]
+            for(let i in matrix) {
+                let items = matrix[i].map(key => poles[key]);
+                if(items.every(itm => itm.innerHTML === "O") || items.every(itm => itm.innerHTML === "X")) {
+                    const markup = `<h1 class="result_banner">Player ${activePlayer} Win!</h1>`;
+                    items.every(itm => itm.style.backgroundColor = '#22dc2e');
+                    restartBtn.style.visibility = "visible";
+                    document.querySelector('.game__table').insertAdjacentHTML('beforeend', markup);
+                    gamePlaying = false;
+                }
+            }
+            if(round >= 8) {
+                const markup = `<h1 class="result_banner">DRAW</h1>`;
                 restartBtn.style.visibility = "visible";
                 document.querySelector('.game__table').insertAdjacentHTML('beforeend', markup);
                 gamePlaying = false;
             }
-        }
-        if(round >= 8) {
-            const markup = `<h1 class="result_banner">DRAW</h1>`;
-            restartBtn.style.visibility = "visible";
-            document.querySelector('.game__table').insertAdjacentHTML('beforeend', markup);
-            gamePlaying = false;
-        }
-        nextPlayer();
+            nextPlayer();
         }
     }
     restartBtn.addEventListener('click', () => {
         init();
         document.querySelector('.result_banner').remove();
     });
+    
+    function testing() {
+        console.log(`${activePlayer}`);
+    }
+    
     function init() {
         activePlayer = 1;
         round = 0;
